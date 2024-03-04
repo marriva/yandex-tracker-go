fe726ea951ca497f6819c9340c2ef5b2c9c19073
Vasily Maryutenkov <vasily.maryutenkov@flant.com>
2024-03-04 12:46:04 +0300

    print errors

--- error.go
+++ error.go
@@ -3,11 +3,14 @@ package tracker
 import "fmt"
 
 type Error struct {
-	Errors        interface{} `json:"errors"`
-	ErrorMessages []string    `json:"errorMessages"`
-	StatusCode    int         `json:"statusCode"`
+	Errors        map[string]interface{} `json:"errors"`
+	ErrorMessages []string               `json:"errorMessages"`
+	StatusCode    int                    `json:"statusCode"`
 }
 
 func (e Error) Error() string {
+	if len(e.Errors) > 0 {
+		return fmt.Sprint(e.Errors)
+	}
 	return fmt.Sprint(e.ErrorMessages)
 }
